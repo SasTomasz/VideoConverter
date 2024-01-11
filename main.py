b13bfac1b185cb39dec1c99e0313d9a34dec5d06
tomasz_sas <tomaszsas.dev@gmail.com>
2024-01-11 09:31:43 +0100

    Add expander to camera_input and file uploader. All images are convert to gray scale and show on screen

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import streamlit as st
+from PIL import Image
 
-st.expander("Expander")
 
-st.camera_input("Camera")
+def convert_to_gray(image):
+    with Image.open(image) as im:
+        gray_image = im.convert("L")
+    return gray_image
+
+
+def main():
+    uploaded_file = st.file_uploader("Upload image")
+
+    with st.expander("Video"):
+        picture = st.camera_input("Camera")
+
+    if uploaded_file:
+        gray_image = convert_to_gray(uploaded_file)
+        st.image(gray_image)
+    elif picture:
+        gray_image = convert_to_gray(picture)
+        st.image(gray_image)
+
+
+if __name__ == "__main__":
+    main()
